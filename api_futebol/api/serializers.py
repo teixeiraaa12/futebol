0d850b14630089d378b6f16468e8a54196bbdd8c
Miguel <miguelramos191207@gmail.com>
2025-03-24 07:57:24 -0300

    Descreva aqui o que foi alterado

--- api_futebol/api/serializers.py
+++ api_futebol/api/serializers.py
@@ -5,8 +5,24 @@ class TimesSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Times
         fields = "__all__"
+        extra_kwargs = {
+            'id': {'help_text': 'Identificador do time'},
+            'nome': {'help_text': 'Nome do time'},
+        }
 
 class PartidaSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Partida
         fields = "__all__"
+        extra_kwargs = {
+            'id': {'help_text': 'Identificador da partida'},
+            'time1': {'help_text': 'ID do primeiro time. Buscar o ID no GET da API de Times'},
+            'time2': {'help_text': 'ID do segundo time. Buscar o ID no GET da API de Times'},
+            'data': {'help_text': 'Data da partida'},
+            'horario': {'help_text': 'Horário da partida'},
+            'placartime1': {'help_text': 'Placar do primeiro time'},
+            'placartime2': {'help_text': 'Placar do segundo time'},
+            'estadio': {'help_text': 'Nome do estádio onde ocorrerá a partida'},
+            'juiz': {'help_text': 'Nome do juiz responsável pela partida'},
+            'quantcartoes': {'help_text': 'Quantidade total de cartões aplicados na partida'},
+        }
--- api_futebol/api/viewsets.py
+++ api_futebol/api/viewsets.py
@@ -1,11 +1,84 @@
 from rest_framework import viewsets
 from api_futebol import models
 from api_futebol.api import serializers
+from drf_yasg.utils import swagger_auto_schema
 
-class TimesViewset (viewsets.ModelViewSet):
+class TimesViewset(viewsets.ModelViewSet):
     serializer_class = serializers.TimesSerializer
-    queryset =models.Times.objects.all()
+    queryset = models.Times.objects.all()
 
-class PartidaViewset (viewsets.ModelViewSet):
+    @swagger_auto_schema(
+        operation_description="Lista todos os times",
+        responses={200: serializers.TimesSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Cria um novo time",
+        responses={201: "Time criado com sucesso"}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Retorna o time conforme o ID informado",
+        responses={200: "Time encontrado"}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Altera o time conforme informado",
+        responses={201: "Time alterado com sucesso"}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Deleta um time",
+        responses={204: "Time deletado com sucesso"}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+
+class PartidaViewset(viewsets.ModelViewSet):
     serializer_class = serializers.PartidaSerializer
     queryset = models.Partida.objects.all()
+
+    @swagger_auto_schema(
+        operation_description="Lista todas as partidas",
+        responses={200: serializers.PartidaSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Cria uma nova partida",
+        responses={201: "Partida criada com sucesso"}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Retorna a partida conforme o ID informado",
+        responses={200: "Partida encontrada"}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Altera a partida conforme informado",
+        responses={201: "Partida alterada com sucesso"}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Deleta uma partida",
+        responses={204: "Partida deletada com sucesso"}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
